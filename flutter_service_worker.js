b05ebbd9e622dcc92b56d29b31857ae6d64b27d7
shadyCS <78737793+shadyCS@users.noreply.github.com>
2023-01-05 14:47:29 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -77,9 +77,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5095a47bc06a194af0b7ca7f27120f77",
-"/": "5095a47bc06a194af0b7ca7f27120f77",
-"main.dart.js": "81ab0a6148af7530247b9f65919470be",
+"index.html": "e7105c83e3f3c0782d8a7ffe7ac098f8",
+"/": "e7105c83e3f3c0782d8a7ffe7ac098f8",
+"main.dart.js": "b0280f45bbd40b3f2af540c025cc7f30",
 "manifest.json": "3f92a849556967510ecd50602f08bc15",
 "version.json": "5e129f52952a920d64681e367187e4ef"
 };
